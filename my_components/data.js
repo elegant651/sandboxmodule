d86a69f9d74309d0d3eb8d07fbccbb26413d01fe
elegantuniv <korea651@gmail.com>
2015-05-31 18:04:45 +0900

    first commit

---
+++ my_components/data.js
@@ -0,0 +1,18 @@
+var temp_app = document.querySelector('#temp_app');
+temp_app.intros = [
+	{
+		img: "../images/dtest.png",
+		title: "Title1",
+		content: "In the sandbox, there's some content."
+	},
+	{
+		img: "../images/dtest.png",
+		title: "Title2",
+		content: "In the sandbox, there's some content."
+	},
+	{
+		img: "../images/dtest.png",
+		title: "Title3",
+		content: "In the sandbox, there's some content."
+	}
+]
